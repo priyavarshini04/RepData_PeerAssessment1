0200c330c1fa9351ba88a98a0a32d48ed64e9012
PRIYAVARSHINI GAJENDRAN RAMESH <58223468+priyavarshini04@users.noreply.github.com>
2020-07-25 19:38:51 +0530

    Create Histogram of the total number of steps taken each day.R

---
+++ Histogram of the total number of steps taken each day.R
@@ -0,0 +1,3 @@
+# draw the histogram
+g <- ggplot(StepsPerDay, aes(Steps))
+g+geom_histogram(boundary=0, binwidth=2500, col="darkgreen", fill="lightgreen")+ggtitle("Histogram of steps per day")+xlab("Steps")+ylab("Frequency")+theme(plot.title = element_text(face="bold", size=12))+scale_x_continuous(breaks=seq(0,25000,2500))+scale_y_continuous(breaks=seq(0,18,2))
